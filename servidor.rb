28c0fb08381b09ae9181ab2c4a7f635d3d5809a8
Clemente Junior <clmnt.jr@gmail.com>
2015-07-02 19:55:45 -0300

    Adicionado análise de requisição
    Quando o servidor não reconhecer o comando ele responderá com a mensagem
    FALHA, caso o contrario responderá com Ok.

--- servidor.rb
+++ servidor.rb
@@ -1,11 +1,17 @@
 require 'socket'
 
 servidor = TCPServer.new(2100)
+tabela_dns = {}
 
 def processar(linha)
-	print('Linha recebida: ')
-	puts(linha)
-	return "Ok"
+	req_conhecidas = ["REG", "IP"] # Requisições conhecidas
+	dados = linha.split(" ")       # Quebra a requisição para melhor analise dos dados
+
+	if req_conhecidas.include? dados[0] then
+		return "Ok"
+	else
+		return "FALHA"
+	end
 end
 
 loop {
